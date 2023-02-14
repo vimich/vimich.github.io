97e319ece279ec2b6dd46ac597f94208816dbd91
vimich <43404631+vimich@users.noreply.github.com>
2023-02-14 18:21:04 +0100

    Update Header.tsx

--- src/containers/home/Header.tsx
+++ src/containers/home/Header.tsx
@@ -42,7 +42,7 @@ const Header: React.FC<{ tp: number }> = props => {
             <TitleAndLogo>
                 {/* This is where you can change the page title */}
                 {/* --------- EDIT LINE BELOW ----------------- */}
-                <H1 data-testid="h1-title">This is the wrong title🤨</H1>
+                <H1 data-testid="h1-title">Github Actions Workshop</H1>
                 {/* ------------------------------------------- */}
                 <Img src={AcnLogo} />
             </TitleAndLogo>
